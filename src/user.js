f50f44d69f495413331b78ffc7ff8683eb576ce1
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-06 11:30:02 -0700

    test code push for dev branch

---
+++ src/user.js
@@ -0,0 +1,18 @@
+export class UserGroup {
+  constructor(groupName) {
+  this.groupName = groupName;
+  this.userIndex = [];
+  }
+  addUser(newUser){
+    this.userIndex.push(newUser)
+  
+  }
+}
+
+export class User {
+  constructor(firstName, lastName, address){
+    let firstName = firstName;
+    let lastName = lastName;
+    let address = address;
+  }
+}
