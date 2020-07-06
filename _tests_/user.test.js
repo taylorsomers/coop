30d26eaf63bc00ef91c33fda95b5f2573d73f195
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-06 15:52:58 -0700

    update use input fields to in clude state, email, linked in and git hub

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -1,11 +1,15 @@
 import {User} from "../src/user.js"
 
 describe('User', () => {
-  test('Should fail if user fields are not Frederick, Ernest, 1234 sunshine blvd, 1233456789', () => {
-    let newUser = new User("Frederick", "Ernest", "1234 Sunshine Blvd", "1233456789");
+  test('Should fail if user fields are not ["Frederick", "Ernest", "1234 Sunshine Blvd", "Oregon", "1233456789", "fake@gmail.com", "linkd", "gitHub"]', () => {
+    let newUser = new User("Frederick", "Ernest", "1234 Sunshine Blvd", "Oregon", "1233456789", "fake@gmail.com", "linkd", "gitHub");
     expect(newUser.firstName).toEqual("Frederick");
     expect(newUser.lastName).toEqual("Ernest");
-    expect(newUser.address).toEqual("1234 Sunshine Blvd");
+    expect(newUser.street).toEqual("1234 Sunshine Blvd");
+    expect(newUser.state).toEqual("Oregon");
     expect(newUser.number).toEqual("1233456789");
+    expect(newUser.email).toEqual("fake@gmail.com");
+    expect(newUser.linkd).toEqual("linkd");
+    expect(newUser.gitHub).toEqual("gitHub");
   });
 });
