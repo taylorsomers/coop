4f9ef12d4d57afec971c4ec5db12a7f0320f65d9
Beverly Potts <pottsbeverly@gmail.com>
2020-07-07 19:28:53 -0400

    Pass test for assigning id to each new usergroup object

--- _tests_/groupCollection.test.js
+++ _tests_/groupCollection.test.js
@@ -9,11 +9,18 @@ describe('GroupCollection', () => {
   test('Should create a GroupCollection object', () => {
     expect(groupCollection.groupIndex).toEqual([]);
   });
-  
+
   test('Should add a group to GroupCollection object', () => {
     
     let myGroup = new UserGroup();
     groupCollection.addGroup(myGroup);
     expect(groupCollection.groupIndex[0]).toEqual(myGroup);
+  });
+
+  test('Should assign an ID to each new UserGroup object', () => {
+    let myGroup = new UserGroup();
+    groupCollection.addGroup(myGroup);
+    expect(groupCollection.currentID).toBe(1);
+    
   });
 });
