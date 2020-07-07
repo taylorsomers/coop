d9524120033d68ba1dfe00c251803e1fc838c502
Beverly Potts <pottsbeverly@gmail.com>
2020-07-07 19:24:07 -0400

    Pass adding group to GroupCollection object test

--- _tests_/groupCollection.test.js
+++ _tests_/groupCollection.test.js
@@ -1,8 +1,19 @@
 import { GroupCollection } from '../src/groupCollection.js';
-
+import { UserGroup } from '../src/userGroup.js';
 describe('GroupCollection', () => {
+  let groupCollection;
+  beforeEach (() => {
+    groupCollection = new GroupCollection();
+  });
+
   test('Should create a GroupCollection object', () => {
-    let groupCollection = new GroupCollection();
     expect(groupCollection.groupIndex).toEqual([]);
+  });
+  
+  test('Should add a group to GroupCollection object', () => {
+    
+    let myGroup = new UserGroup();
+    groupCollection.addGroup(myGroup);
+    expect(groupCollection.groupIndex[0]).toEqual(myGroup);
   });
 });
