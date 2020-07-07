20e25f58860934835a68c325d18886db89f26d41
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-07 14:38:22 -0700

    create working API call from yelp with headers utilizing async exported functions. Will include error handling

---
+++ yelpCall.js
@@ -0,0 +1,33 @@
+export class BankInfo {
+  async getBankInfo() {
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Accept", "application/json");
+      myHeaders.append("Authorization", "Bearer rO557Y2VDrYgrPcSEoLdKPSoTUG4hnDkOrS6mPtlDl3Qx1NcRH24Pv3s6KPdmFe2xeuBs-jQvQ1BgL6EtOi38H4Wqijt7EpG8CPOQADYLz2ifAEUCLDffH6W9LcEX3Yx");
+    
+      const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      let response = await fetch("https://api.yelp.com/v3/businesses/search?location=OR&catagories=Banks&limit=3&rating=4&term=credit%20union", requestOptions)
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+        console.log(jsonifiedResponse);
+      } else {
+        console.log(response.statusText);
+        // jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
+(async () => {
+  let bankInfo = new BankInfo();
+  const response = await bankInfo.getBankInfo();
+  console.log(response);
+})();
