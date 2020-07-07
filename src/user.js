f256a1bde8035144fc582892e25e08997b9e2b48
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-07 10:27:44 -0700

    add working zip code feild to tests

--- src/user.js
+++ src/user.js
@@ -1,9 +1,10 @@
 export class User {
-  constructor(firstName, lastName, street, state, number, email, linkd, gitHub){
+  constructor(firstName, lastName, street, state, zip, number, email, linkd, gitHub){
     this.firstName = firstName;
     this.lastName = lastName;
     this.street = street;
     this.state = state;
+    this.zip = zip;
     this.number = number;
     this.email = email;
     this.linkd = linkd;
