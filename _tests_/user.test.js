111573f26c59047aa628b08eff5b06e6e0eee0c0
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-07 09:29:38 -0700

    seperate logics in to seperate files

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -14,10 +14,3 @@ describe('User', () => {
   });
 });
 
-describe('numPass', () => { 
-  test ('should fail if a user inputs anything but a 9 digit number for number', () => {
-    let newNumber = 1234567890;
-    let answer = numPass(newNumber);
-    expect(answer).toEqual(false);
-  });
-});
