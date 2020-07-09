364eb001688b04f597396d3cbf0714747b179182
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-09 12:11:53 -0600

    delet mail.js.
    co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>
    co-authored-by: Beverly Potts pottsbeverly@gmail.com
    co-authored-by: Taylor Somers < tsomers@yandex.com>

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import { UserGroup } from './userGroup.js';
 function getGroup(groupID) {
   let groupString = localStorage.getItem("group");
   let group = JSON.parse(groupString);
-  if (group.id === groupID){
+  if (group.id === groupID) {
     return group;
-  } else { 
+  } else {
     alert("No Match for Group ID");
   }
 }
@@ -32,7 +32,7 @@ function storeGroup(group) {
 }
 // New User - New Group Creation
 $(document).ready(function () {
-  $("#create-group").submit(function(event) {
+  $("#create-group").submit(function (event) {
     event.preventDefault();
     const firstName = $("#firstName").val();
     const lastName = $("#lastName").val();
@@ -50,26 +50,26 @@ $(document).ready(function () {
 
 
     let user = new User(firstName, lastName, street, city, state, zip, phone, email, linkedIn, gitHub);
-    console.log(user);console.log(user);
+    console.log(user);
     storeSessionUser(user);
     let group = new UserGroup(agent, groupName, regiAddress);
     group.generateGroupID();
     group.addUser(user);
     storeGroup(group);
-    
+
     $("#create-group").hide();
     $("#createOutput").html(`<li> ${group.id}</li> <li> ${groupName}</li> <li> ${firstName} + ${lastName}</li>`);
     let userOutput = user.stateCheck();
     $("#createOutput").append(userOutput[0]);
     $("#createOutput").append(userOutput[1]);
     $("#createOutput").append(userOutput[2]);
-  //output to include location specific information links
-  //send yourself pertinent information via email.
+    //output to include location specific information links
+    //send yourself pertinent information via email.
   });
 });
 
 $(document).ready(function () {
-  $("#join-group").submit(function(event) {
+  $("#join-group").submit(function (event) {
     event.preventDefault();
 
     const firstName = $("#firstName").val();
@@ -90,7 +90,7 @@ $(document).ready(function () {
     let myGroup = getGroup(groupID);
     myGroup.addUser(user);
     storeGroup(myGroup);
-    
+
     $("#join-group").hide();
     $("#joinOutput").html(`<li> ${myGroup.groupName}</li> <li>${firstName} + ${lastName}</li>`);
     let userOutput = user.stateCheck();
@@ -98,8 +98,8 @@ $(document).ready(function () {
     $("#createOutput").append(userOutput[1]);
     $("#createOutput").append(userOutput[2]);
 
-  //output to include location specific information links
-  //send yourself pertinent information via email.
+    //output to include location specific information links
+    //send yourself pertinent information via email.
 
   });
 });
--- src/user.js
+++ src/user.js
@@ -1,5 +1,5 @@
 export class User {
-  constructor(firstName, lastName, street, city, state, zip, phone, email, linkedIn, gitHub){
+  constructor(firstName, lastName, street, city, state, zip, phone, email, linkedIn, gitHub) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.street = street;
@@ -11,15 +11,15 @@ export class User {
     this.linkedIn = linkedIn;//change from 'linkd' to 'linkedIn'
     this.gitHub = gitHub;
   }
-  stateCheck(){
+  stateCheck() {
     let orBizCenter = "https://bizcenter.org/";
     let secStateOR = "https://sos.oregon.gov/business/Pages/domestic-limited-liability-companies-forms.aspx";
     let aMalan = ["Audry", "Malan", "Cottage Grove, OR", "(307) 717 2559"];
     let orArr = [secStateOR, orBizCenter, aMalan];
-    if (this.state === "Oregon"){
+    if (this.state === "Oregon") {
       return (orArr);
 
-    } else if (this.state === "Washington"){
+    } else if (this.state === "Washington") {
       let waProjEq = "https://www.project-equity.org/wp-content/uploads/2019/09/Project-Equity-Washington-State-Small-Business-Closure-Crisis.pdf";
       let secStateWA = "https://www.sos.wa.gov/corps/limitedliabilitycompaniesllconlineandpaperregistrations.aspx";
       let waDevCenter = "https://wsbdc.org/";
